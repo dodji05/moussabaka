0326db59f5f46d0d445226e11709b7efb25bbd2f
kabadoul <49312043+kabadoul@users.noreply.github.com>
2023-12-25 13:05:49 +0100

    ajout recherche

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,22 +2,72 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Repository\CandidatRepository;
+use Knp\Component\Pager\PaginatorInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(CandidatRepository $candidatRepository,PaginatorInterface $paginator): Response
+    public function index(CandidatRepository $candidatRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $candidats=$candidatRepository->findAll();
+        $searchForm = $this->createForm(SearchType::class);
+        $searchForm->handleRequest($request);
+
+        $queryBuilder = $candidatRepository->createQueryBuilder('c');
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $searchTerm = $searchForm->get('search')->getData();
+
+            // Modify the queryBuilder to include your search logic
+            if ($searchTerm) {
+                $queryBuilder
+                    ->innerJoin('c.categorie','s')
+                    ->innerJoin('c.ecoledeProvenance','e')
+                    ->orWhere('c.mom LIKE :searchTerm')
+                    ->orWhere('c.prenom LIKE :searchTerm')
+                    ->orWhere('c.numeroCandidat LIKE :searchTerm')
+                    ->orWhere('c.age LIKE :searchTerm')
+                    ->orWhere('s.name LIKE :searchTerm')
+                    ->orWhere('c.localite LIKE :searchTerm')
+                    ->orWhere('e.nom LIKE :searchTerm')
+                    ->setParameter('searchTerm', "%$searchTerm%");
+            }
+        }
+
+        $candidats = $queryBuilder->getQuery()->getResult();
+
+        // Paginate the results using KnpPaginatorBundle
+        $pagination = $paginator->paginate(
+            $candidats,
+            $request->query->getInt('page', 1),
+            10 // Number of items per page
+        );
+
         return $this->render('index.html.twig', [
-            'candidats'=>$candidats,
-
-
+            'pagination' => $pagination,
+            'searchForm' => $searchForm->createView(),
         ]);
     }
+//    public function index(CandidatRepository $candidatRepository,PaginatorInterface $paginator,Request $request): Response
+//    {
+//        $candidats=$candidatRepository->findAll();
+//        $pagination=$paginator->paginate(
+//            $candidats,
+//            $request->query->getInt('page',1),
+//            10
+//        );
+//        return $this->render('index.html.twig', [
+//            'candidats'=>$candidats,
+//            'pagination'=>$pagination
+//
+//
+//        ]);
+//    }
 }
---
+++ src/Form/SearchType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('search', TextType::class, [
+                'label' => 'Chercher',
+                'required' => false,
+            ])
+
+
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
